1b1bac96da46db73de168f356120d559a7784401
TheNileshGarg <engineer.nileshgarg@gmail.com>
2024-05-24 22:34:36 +0530

    Variables and Data Types

---
+++ varaibles.py
@@ -0,0 +1,26 @@
+# Variables is basically a name that is used to denote a  space in RAM 
+# This space basically stores any value we want 
+
+# Python is dynamically typed language i.e you do not need to explicitly declare data types 
+
+a = "Kevin" # String 
+b = 123456789 # number 
+c = True # boolean 
+d = 1.412
+# Data types are needed to specify how a data is stored - size and procedure 
+# Also operations differ based on data types 
+# "22" + "22" = "2222"
+# 22 + 22 = 44
+
+print(a, b, c, d, sep = '\n')
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+# int, float, string, boolean, complex
+# Sequenced Data - List, Tuple 
+# List - mutable, tuple - immutable
+# Mapped Data - Dictionary 
+
+# Everything in Python is an object - class object, strings, number .... 

f4995e942e4760d63585bf2c5c7577a8ab2f3091
TheNileshGarg <engineer.nileshgarg@gmail.com>
2024-05-25 00:09:58 +0530

    User Intake and Better Calculator using user input

---
+++ user_intake.py
@@ -0,0 +1,10 @@
+# input() functions reads user input from console and returns it as a string 
+name = input("Please enter your name. ")
+print("My name is", name)
+
+age = int(input("Please enter your age (in years) "))
+print("I am", age, "years old")
+
+# If we do not type cast, then user input will show the behavior of string as it is a string 
+# You will learn error handling in future so that you can handle if user enters different type than 
+# .. one you want to typecast 

80a60c572b54137931e2133b20060c4dc13fd16c
TheNileshGarg <engineer.nileshgarg@gmail.com>
2024-05-24 22:15:22 +0530

    Comments and Escape Characters

---
+++ comments_and_escape.py
@@ -0,0 +1,19 @@
+## \n to add a new line while printing 
+print("Python is an amazing lanuage \nC++ is also an amazing language ")
+
+# - To write single line comments 
+''' To write mutliline comments '''
+
+# Comments are lines which are written for description but not executed 
+
+# Select multiple lines and press ctrl + / to comment multiple lines 
+print("Hello World") # You can write a comment even in same line as code 
+
+# Escape characters - you use them to have characters you can not directly have 
+# \n - new line 
+# \" - quotes 
+
+print("End", end = '-')
+print("Game")
+# By default, end is \n 
+# Seperator Argument is used when we pass multiple objects to print function 

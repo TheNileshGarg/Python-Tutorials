bc9d372832ae5d74c3e8dde13f288be688026e34
TheNileshGarg <engineer.nileshgarg@gmail.com>
2024-05-24 14:53:04 +0530

    Intro to python
    A python file with comments that are used to briefly introduce python to us

---
+++ python-intro.py
@@ -0,0 +1,31 @@
+## Python 
+
+# Download python from the browser and then you may use an IDE 
+# Replit -A cloud based IDE for computers with low compute 
+
+# Programming - A way to tell computers what to do 
+
+# Python is a high level, dynamically typed, platform independent programming language which 
+# ... supports both functional and object oriented programming 
+
+# Python is open source, widely used programming language which can integrate with other languages.
+
+# Python is used in web sites, databases, data science, AI and ML. 
+# In windows, remeber to add the path variable 
+
+## Amazing Python Projects To Build 
+
+# 1. Jarvis - Virtual Assistant 
+# 2. Love Calculator 
+# 3. Face Recognition Program 
+# 4. Flappy Bird Game and AI Agent 
+# 5. Snake Game 
+
+# Sometimes, You may use some platform dependent APIs so you need to be careful while building projects 
+
+## Modules and pip 
+# Modules - Using others code to build your own system 
+# pip - A package manager (basically to bring certain modules from internet to your system)
+
+# Replit automatically installs certain modules if not available on importing 
+# Some modules are built in python and they do not need to be installed 

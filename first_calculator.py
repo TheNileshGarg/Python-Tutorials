68bd296ff99ff5893caa57e8addb01b3c6bc49df
TheNileshGarg <engineer.nileshgarg@gmail.com>
2024-05-24 23:05:21 +0530

    First Calculator

---
+++ first_calculator.py
@@ -0,0 +1,15 @@
+# + - * / // % - all airthemtic operators and more are available in python 
+# ** - power operator 
+
+a = 2 
+b = 10 
+
+print("The Sum of two numbers a and b is", a+b)
+print("The Difference of two numbers a and b is", a-b)
+print("The Product of two numbers a and b is", a*b)
+print("a divided by b is", a/b)
+print("Quotient of a on divsion by b is", a//b)
+print("Remainder of a on divsion by b is", a%b)
+print("a raised to power b is", a**b)
+
+

a034965879bbc8dd43def9679580c3f200d39be3
TheNileshGarg <engineer.nileshgarg@gmail.com>
2024-05-28 20:59:23 +0530

    Short Hand If Else In Python

---
+++ short_hand_ifelse.py
@@ -0,0 +1,14 @@
+# if condition is simple and code to be executed is short 
+# we can use short hand if else notation 
+
+a = 100
+b = 20 
+
+print("a") if a > b else print("=") if a == b else print("b")
+
+c = 9 if a > b else 0
+print(c)
+
+# result = value_if_true if condition else value_if_false 
+
+# For good readability, one use short hand notation where we have simple conditons 

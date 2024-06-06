86b2323f0a2f9d454df77b2134750080273ef752
TheNileshGarg <engineer.nileshgarg@gmail.com>
2024-06-06 23:17:58 +0530

    Shutil Module

---
+++ Shutil_Module.py
@@ -0,0 +1,13 @@
+## Shutil Module 
+# Shell Utility Module 
+# Built in modue which provides High Level Interface for working with files and directories 
+
+# shutil.copy(src, dst) - copies a file at src to a destination at dst
+# shutil.copy2(src, dst) - similar to copy but preserves meta data like time stamp 
+
+# shutil.copytree(src, dest) - Recursively copies directory located at src to a new location 
+# specified by src. If destination already exists, directory will be merged with it. 
+
+# shutil.move(src, dest) - moves a file from source to destination 
+# shutil.retree(path) - recursively deletes an entire directory 
+
